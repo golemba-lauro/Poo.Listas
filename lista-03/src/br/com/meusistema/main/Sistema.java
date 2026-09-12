5b88bcda636daec457cf23d0036ed653276508d0
Lauro <golembalauro@gmail.com>
2026-09-12 09:11:34 -0300

    feat(lista-03): estrutura de pacotes e classes Carro e Sistema

---
+++ lista-03/src/br/com/meusistema/main/Sistema.java
@@ -0,0 +1,19 @@
+package br.com.meusistema.main;
+
+import br.com.meusistema.model.Carro;
+
+public class Sistema {
+    public static void main(String[] args) {
+        System.out.println("Iniciando o sistema...");
+
+        Carro meuCarro = new Carro();
+        meuCarro.modelo = "Fusca";
+        meuCarro.cor = "Azul";
+        meuCarro.buzinar();
+
+        Carro segundoCarro = new Carro();
+        segundoCarro.modelo = "Uno";
+        segundoCarro.cor = "Vermelho";
+        segundoCarro.buzinar();
+    }
+}
