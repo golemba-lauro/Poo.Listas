a9303c293696012faf03598ab29b2351309d0b4c
Lauro <golembalauro@gmail.com>
2026-09-14 20:22:38 -0300

    feat:implementa tratamento de excecoes com try-catch, finally e excecoes customizadas

---
+++ lista-11/src/br/com/nexustech/exception/NivelInsuficienteException.java
@@ -0,0 +1,10 @@
+package br.com.nexustech.exception;
+
+public class NivelInsuficienteException extends RuntimeException{
+
+    public NivelInsuficienteException() {
+        super("Seu nivel e muito baixo para esta masmorra!");
+
+    }
+    
+}
