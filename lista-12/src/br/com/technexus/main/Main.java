03157971e877d63ae6b792de5e6fdd170ce98085
Lauro <golembalauro@gmail.com>
2026-09-14 21:11:23 -0300

    feat: relatorios com streams e funcoes lambda

---
+++ lista-12/src/br/com/technexus/main/Main.java
@@ -0,0 +1,28 @@
+package br.com.technexus.main;
+
+import br.com.technexus.model.Loja;
+import br.com.technexus.model.Produto;
+
+public class Main {
+    public static void main(String[] args){
+
+    Loja loja = new Loja();
+    
+    loja.cadastrar(new Produto("The Witcher", "GAMES", 150.0));
+    loja.cadastrar(new Produto("FIFA", "GAMES", 200.0));
+    loja.cadastrar(new Produto("Java for Dummies", "LIVROS", 100.0));
+    loja.cadastrar(new Produto("Clean Code", "LIVROS", 80.0));
+    loja.cadastrar(new Produto("Mouse", "HARDWARE", 50.0));
+
+    System.out.println("--- GAMES ---");
+    System.out.println(loja.buscarPorCategoria("GAMES"));
+
+    System.out.println("--- PATRIMONIO TOTAL ---");
+    System.out.println(loja.calcularPatrimonioTotal());
+
+    System.out.println("--- TOTAL LIVROS ---");
+    System.out.println(loja.calcularTotalPorCategoria("LIVROS"));
+
+
+    }
+}
